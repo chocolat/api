92c1b827964fe5aa7d225569634e8a934a4bacdd
Nicholas Penree <nick@penree.com>
2012-08-30 20:56:44 -0400

    Support loading via package.json and it's `main` key

--- chocolat.js
+++ chocolat.js
@@ -1,13 +1,10 @@
 var path = require('path');
+var fs = require('fs');
 
 global.current_mixin_name = null;
 
 // This line is important! It sets up the globals for the api
 require('api');
-
-
-// var os = require('os');
-// console.log("Bite my shiny metal ass [" + os.platform() + " " + os.release() + "]");
 
 // Temporary way to stop node.js quitting immediately until we add an events.EventEmitter
 process.stdin.resume();
@@ -45,27 +42,42 @@ global.call_function_as = function(mixinname, func, args) {
 };
 
 global.load_initjs = function(mixinPath, mixinID) {
-    
+
     // Look in loadedMixins for this mixin
     // Remove any that match mixinPath
     loadedMixins = loadedMixins.filter(function (aMixin) { return aMixin.path !== mixinPath; });
-    
+
     // Read the source from disk
-    var pathToInitjs = path.join(mixinPath, "init.js");
+    var pathToMain = path.join(mixinPath, "init.js");
+    var pathToPackageJson = path.join(mixinPath, "package.json");
+
+    // Try to use package.json if it exists
+    if (fs.existsSync(pathToPackageJson)) {
+        try {
+            var json = JSON.parse(fs.readFileSync(pathToPackageJson, "utf8"));
+            if (typeof json !== "undefined" && typeof json.main !== "undefined") {
+                pathToMain = path.join(mixinPath, json.main);
+            }
+        } catch(e) {
+            // Parsing failed? Looks like we are using init.js
+        }
+    }
     
     // Add our new Mixin    
     loadedMixins.push({ "path": mixinPath, "id": mixinID });
-    
+
     // Remove from the module cache
     var keys = Object.keys(require.cache);
     for (var i = 0; i < keys.length; i++) {
         var key = keys[i];
         delete require.cache[key];
     }
-    
+
     // Run the code!
     try {
-        require(pathToInitjs);
+        if (fs.existsSync(pathToMain)) {
+            require(pathToMain);            
+        }
     }
     finally {
         // Remember, the above line may throw, so put anything else here
